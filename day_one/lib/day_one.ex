629ac94da7a489f67599086f402ad9fbbc3943fc
Sauronius <cisekdmn@gmail.com>
2025-12-01 16:00:27 +0000

    Day one finished.

--- day_one/lib/day_one.ex
+++ day_one/lib/day_one.ex
@@ -1,18 +1,69 @@
 defmodule DayOne do
   @moduledoc """
-  Documentation for `DayOne`.
-  """
+  Documentation for 'DayOne' of Advent of Code 2025.
 
-  @doc """
-  Hello world.
+  ## Input files location
+  My input files sit in "../../AoC-2025-inputs/inputX.txt" path, so getting solutions
+  with 'mix test' requires corrections in day_one_test.exs.
 
   ## Examples
 
-      iex> DayOne.hello()
-      :world
+      iex> DayOne.part_one("path/to/your/input")
+      1102
 
+      iex> DayOne.part_two("path/to/your/input")
+      6175
   """
-  def hello do
-    :world
+  @hundred_range 0..99//1
+
+  def prepare_file(path) do
+    path
+    |> File.read!()
+    |> String.trim_trailing("\n")
+    |> String.split("\n")
+    |> Enum.map(&String.split_at(&1, 1))
+    |> Enum.map(fn {direction, string_distance} -> distance = {direction, String.to_integer(string_distance)} end)
   end
+
+  def part_one(path) do
+    path
+    |> prepare_file()
+    |> Enum.reduce({50, 0}, fn rotation, acc -> {new_number, _} = turn_dial(elem(acc, 0), rotation)
+      if new_number == 0 do
+        {new_number, elem(acc, 1) + 1}
+      else
+        {new_number, elem(acc, 1)}
+      end end)
+    |> elem(1)
+  end
+
+  def part_two(path) do
+    path
+    |> prepare_file()
+    |> execute_rotations(50, 0)
+    |> elem(1)
+  end
+
+  defp execute_rotations([], number, acc), do: {number, acc}
+  defp execute_rotations([{"L", distance} | rest], number, acc) do
+    all_clicks = Stream.cycle(@hundred_range)
+    |> Enum.take((div(distance, 100) + 1) * 100 + number)
+    |> Enum.slice(-distance..-1//1)
+
+    new_number = List.first(all_clicks)
+    zeros = Enum.count(all_clicks, fn x -> x == 0 end)
+    execute_rotations(rest, new_number, acc + zeros)
+  end
+  defp execute_rotations([{"R", distance} | rest], number, acc) do
+    all_clicks = Stream.cycle(@hundred_range)
+    |> Enum.take(distance + number + 1)
+    |> Enum.slice(number + 1..-1//1)
+
+    new_number = List.last(all_clicks)
+    zeros = Enum.count(all_clicks, fn x -> x == 0 end)
+    execute_rotations(rest, new_number, acc + zeros)
+  end
+
+  defp turn_dial(start_number, {"L", distance}), do: if( (new_number = start_number - rem(distance, 100)) < 0, do: {new_number + 100, 1}, else: {new_number, 0})
+  defp turn_dial(start_number, {"R", distance}), do: if( (new_number = start_number + rem(distance, 100)) > 99, do: {new_number - 100, 1}, else: {new_number, 0})
 end
--- day_one/test/day_one_test.exs
+++ day_one/test/day_one_test.exs
@@ -1,8 +1,35 @@
 defmodule DayOneTest do
   use ExUnit.Case
-  doctest DayOne
 
-  test "greets the world" do
-    assert DayOne.hello() == :world
+  setup_all do
+    file_content = """
+    L68
+    L30
+    R48
+    L5
+    R60
+    L55
+    L1
+    L99
+    R14
+    L82
+    """
+
+    File.write!("testfile.txt", file_content)
+    on_exit(fn -> File.rm!("testfile.txt") end)
+  end
+
+  test "part one test" do
+    assert DayOne.part_one("testfile.txt") == 3
+  end
+  test "part one solution" do
+    assert DayOne.part_one("../../AoC-2025-inputs/input1.txt") == :solution1
+  end
+
+  test "part two test" do
+    assert DayOne.part_two("testfile.txt") == 6
+  end
+  test "part two solution" do
+    assert DayOne.part_two("../../AoC-2025-inputs/input1.txt") == :solution2
   end
 end
