24a36dc29f3105a9c2be33baa85ed2552dd05b45
Sauronius <cisekdmn@gmail.com>
2025-12-01 05:58:21 +0000

    Init commit

---
+++ day_one/lib/day_one.ex
@@ -0,0 +1,18 @@
+defmodule DayOne do
+  @moduledoc """
+  Documentation for `DayOne`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DayOne.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
